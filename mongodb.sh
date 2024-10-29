571dceed4e16a7e545dacb3879dbe2fbf7a721ee
prasad <prasad@gmail.com>
2024-10-29 21:38:22 +0530

    robo-shop

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -9,3 +10,13 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
