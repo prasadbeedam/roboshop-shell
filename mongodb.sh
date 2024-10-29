0380735dbf447226b9ee0c1e38b8e7a5a6409e1d
prasad <prasad@gmail.com>
2024-10-29 21:30:18 +0530

    robo-shop

---
+++ mongodb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
